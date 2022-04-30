284180ca625e26dc814e85a3d2585721a451a747
Animesh <animeshkr29@gmail.com>
2022-04-30 13:39:44 +0530

    Adding some todo tasks

---
+++ cypress/integration/automationPractice/jsonWriting.spec.js
@@ -0,0 +1,22 @@
+describe('Read Json with static path', () => {
+    /*
+    Rerading Json with dynamic Path Json
+
+    */
+
+})
+describe('Read Json with Dynamic path', () => {
+    /*
+    reading Json with dynamic Path Json
+
+    */
+
+})
+
+describe('Edit Json file and Write it again in run time', () => {
+    /*
+   Edit Json in run time and save its again
+
+    */
+
+})
---
+++ cypress/integration/automationPractice/login.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Ui Plain Login', () => {
+    /*
+    This Block to be used for Plain Login ,for Ex
+    iuser Logged in with His/her credetnails and 
+    validate anyelement after Login Sucessfull
+
+    */
+
+})
+
+describe('Ui Login and Saving Tokens', () => {
+  /*
+  This Block to be used for Plain Login ,for Ex
+  User Login with credentails and storing the cookies 
+  and save in Json file ,so that in later stage that Login token 
+  can be used for other API call or Login redirection 
+
+  */
+
+})
+describe('Login without Ui with Token ', () => {
+  /*
+  This Block to be used for With out Ui login but user able 
+  to redirect to any page by using toekn 
+  So its something like , first attempt login with Ui 
+  take token and same in Json, Hit other URL with intercpet()
+  and pass token and check if URL able to access or not 
+
+  */
+
+})
